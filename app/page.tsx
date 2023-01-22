322d49ed1e1b1755b1556d8e706af6ed4d9305dc
Romuru <pastelblue0721@gmail.com>
2023-01-22 16:10:38 +0900

    feat (tailwind,  daisyUI install)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-"use client";
-
 export default function Home() {
-  return <div>asdd</div>;
+  return (
+    <>
+      <span className="badge badge-outline badge-lg">main</span>
+      <div className="kbd">it's work!</div>
+      <button className="btn-info btn px-8 loading">info</button>
+    </>
+  );
 }
