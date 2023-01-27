269b1388d2f87963d6f68ae41d7e39b17748a225
Romuru <pastelblue0721@gmail.com>
2023-01-27 23:40:46 +0900

    solve(layout.tsx) hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      {children}
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <>
-      <span className="badge badge-outline badge-lg">main</span>
-      <div className="kbd">it's work!</div>
-      <button className="btn-info btn px-8 loading">info</button>
+      <div>
+        <span className="badge badge-outline badge-lg">main</span>
+        <div className="kbd">it's work!</div>
+        <button className="px-8 btn-info btn loading">info</button>
+      </div>
     </>
   );
 }
