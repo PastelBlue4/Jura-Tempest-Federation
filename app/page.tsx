7763b85a706590f34c1f91c3b7513c2857e5cba4
Romuru <pastelblue0721@gmail.com>
2023-02-01 23:59:38 +0900

    feat head routing

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <>
       <div>
+        <head title="Romuru Tech Blog" />
         <span className="badge badge-outline badge-lg">main</span>
         <div className="kbd">it's work!</div>
         <button className="px-8 btn-info btn loading">info</button>
