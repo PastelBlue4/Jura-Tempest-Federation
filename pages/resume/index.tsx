43eec608f023d6a9f8ea65cfe0430b00f0d97e17
Romuru <pastelblue0721@gmail.com>
2023-02-18 14:41:56 +0900

    resume, portfolio routing

---
+++ pages/resume/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react";
+
+export default function resume() {
+  return (
+    <>
+      <Head>
+        <title>Romuru Resume | Jura Tempes Federation</title>
+        <meta name="description" content="Romuru's Resume" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon.jpg" />
+      </Head>
+      <h1>Hi, I&#39;m Romuru!</h1>
+
+      <span className="">&gt;</span>
+    </>
+  );
+}
