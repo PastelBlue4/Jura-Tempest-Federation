772154ba867cf305816582d70e163f9820d678fd
Romuru <pastelblue0721@gmail.com>
2024-08-28 00:38:04 +0900

    refactor : separate MDX-Viewer, CodeBlock components

---
+++ app/_components/CodeBlock/CodeBlock.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import csharp from "react-syntax-highlighter/dist/cjs/languages/prism/csharp";
+import javascript from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
+import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
+import rust from "react-syntax-highlighter/dist/cjs/languages/prism/rust";
+import java from "react-syntax-highlighter/dist/cjs/languages/prism/java";
+import go from "react-syntax-highlighter/dist/cjs/languages/prism/go";
+
+export default function CodeBlock() {
+  const codeSyntax = "code code code ";
+  return (
+    <>
+      <SyntaxHighlighter language="javascript" style={oneLight} showLineNumbers>
+        {codeSyntax}
+      </SyntaxHighlighter>
+    </>
+  );
+}
--- app/_components/MDX_Viewer/MDX_Viewer.tsx
+++ app/_components/MDX_Viewer/MDX_Viewer.tsx
@@ -1,11 +1,7 @@
+"use client";
+
 import React from "react";
 
-import TestMarkdown from "../../../public/posts/1234.mdx";
-
 export default function MDX_Viewer() {
-  return (
-    <>
-      <TestMarkdown />
-    </>
-  );
+  return <></>;
 }
