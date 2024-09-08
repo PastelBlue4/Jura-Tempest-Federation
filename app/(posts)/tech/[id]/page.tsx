ff257f767f6a59859dd9042f95a3590ce27d9df7
Romuru <pastelblue0721@gmail.com>
2024-09-08 23:57:56 +0900

    chore : new api file

--- app/(posts)/tech/[id]/page.tsx
+++ app/(posts)/tech/[id]/page.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import TestMDX from "../../../../posts/test1.mdx";
-
 export default async function Page({ params }: { params: { id: string } }) {
   console.log(params);
-  return (
-    <div>
-      <TestMDX />
-    </div>
-  );
+  return <div></div>;
 }
