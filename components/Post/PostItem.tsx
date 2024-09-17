a0b1a0f578141e92f1c68b8d601c9a62d5c9d2a6
Romuru <pastelblue0721@gmail.com>
2024-09-17 18:04:16 +0900

    style : postItem 제목, 설명 폰트 수정

--- components/Post/PostItem.tsx
+++ components/Post/PostItem.tsx
@@ -12,26 +12,28 @@ export default function PostItem({ post }: Props) {
     <>
       <Link
         href={`/post/${post.slug}`}
-        className="flex flex-col items-center justify-between w-full h-auto max-w-sm mx-auto rounded-lg "
+        className="flex flex-col items-center justify-between w-full h-auto max-w-sm mx-auto "
       >
         <div className="w-full overflow-hidden">
-          <div className="relative w-full h-64 transition-transform bg-blue-300 rounded-t-md hover:scale-105">
+          <div className="relative w-full h-64 transition-transform bg-blue-200 rounded-t-sm hover:scale-110">
             <Image
               src={post.ogImage ? post.ogImage : "/post-asset/default-og.jpeg"}
               fill
               alt=""
-              className="object-cover"
+              className="object-cover rounded-t-sm"
             />
           </div>
         </div>
 
-        <div className="flex flex-col justify-between w-full h-32 px-3 py-3 bg-white rounded-b-md">
-          <span className="text-xl font-semibold text-gray-900 break-keep">
-            {post.title}
-          </span>
-          <span className="h-full overflow-hidden text-base text-gray-500">
-            {post.description}
-          </span>
+        <div className="flex flex-col justify-between w-full h-40 px-3 py-3 bg-white rounded-b-sm">
+          <div className="flex flex-col gap-y-1">
+            <span className="text-lg font-semibold text-gray-900 break-keep">
+              {post.title}
+            </span>
+            <span className="h-full overflow-hidden text-sm text-gray-500">
+              {post.description}
+            </span>
+          </div>
 
           <div className="flex items-center justify-between space-x-1 text-sm text-gray-500 ">
             <div className="flex gap-x-1">
